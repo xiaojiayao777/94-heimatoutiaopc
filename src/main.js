ce5707d3230c8f80301788addebd9898a0d2138e
xiaojiayao777 <1392684833@qq.com>
2020-03-03 12:25:05 +0800

    Elementui安装

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入模块
+import ElementUI from 'element-ui'
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+// 全局注册，一旦全局注册可在任何地方使用组件
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
