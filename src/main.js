90da270cb2097bf0bb221e5bf3398271996a8044
xiaojiayao777 <1392684833@qq.com>
2020-03-03 13:15:25 +0800

    准备资源

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router'
 import ElementUI from 'element-ui'
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入默认样式
+import '@/styles/index.less'
 
 Vue.config.productionTip = false
 // 全局注册，一旦全局注册可在任何地方使用组件
+// vue.use实际上会调用element UI的install方法，并且传入当前的Vue对象
 Vue.use(ElementUI)
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+// 用一种简写的方式替换原有的方式
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    // 强制跳转到home页
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: 'home'
+
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
+  // {
+  //   // 按需加载
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
