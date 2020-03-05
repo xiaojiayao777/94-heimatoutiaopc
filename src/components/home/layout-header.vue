db7c473b22cff9fcd00cfe4c6c846d17ef1151b0
xiaojiayao777 <1392684833@qq.com>
2020-03-05 19:05:47 +0800

    主页-头部退出系统-问题：不能获取用户信息

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -9,16 +9,16 @@
      <el-col class="right" :span="12">
        <el-row type="flex" justify="end" align="middle">
          <img :src="userInfo.photo" alt="">
-         <!-- 下拉菜单 -->
-         <el-dropdown trigger="click">
+         <!-- 下拉菜单 点击菜单项出发command事件-->
+         <el-dropdown trigger="click" @command="clickMenu">
             <!-- 下拉菜单显示内容 -->
             <span>{{userInfo.name}}</span>
             <!-- 下拉内容需要做具名插槽dropdown   el-dropdown-menu是专门做下拉内容效果的组件-->
             <el-dropdown-menu slot="dropdown" >
-               <!-- 下拉选项 el-dropdown-item是专门做下拉选项的组件 -->
-               <el-dropdown-item>个人信息</el-dropdown-item>
-               <el-dropdown-item>git地址</el-dropdown-item>
-               <el-dropdown-item>退出</el-dropdown-item>
+               <!-- 下拉选项 el-dropdown-item是专门做下拉选项的组件  给每一个选项一个command属性-->
+               <el-dropdown-item  command="info">个人信息</el-dropdown-item>
+               <el-dropdown-item command="git">git地址</el-dropdown-item>
+               <el-dropdown-item command="lgout">退出</el-dropdown-item>
 
             </el-dropdown-menu>
 
@@ -34,7 +34,27 @@ export default {
   data () {
     return {
       // y用户个人信息
-      userInfo: {}
+      userInfo: {
+        // name: '22'
+      }
+    }
+  },
+  methods: {
+    clickMenu (command) {
+      // alert('22' + command)
+      // 需要处理三种情况
+      if (command === 'info') {
+        // 点击了个人信息
+      } else if (command === 'git') {
+        // 点击了git地址就跳转到git仓库 改变当前项目地址
+        window.location.href = 'https://github.com/xiaojiayao777/94-heimatoutiaoPC'
+      } else {
+        // 退出系统 删除token 跳转回登陆页
+        // 删除localStorage中的某个选项缓存
+        window.localStorage.removeItem('user-token')
+        // 跳回登录页面 编程式导航
+        this.$router.push('/login')
+      }
     }
   },
   created () {
