0e95d876d7ee6aced09dd8e4fa3a99e288b58136
xiaojiayao777 <1392684833@qq.com>
2020-03-06 22:43:23 +0800

    黑马pc-评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,13 +8,28 @@
         评论管理
       </template>
    </bread-crumb>
+   <!-- 表格组件 -->
+   <el-table :data="list">
+      <!-- 使用el-table-column作为列 -->
+      <!-- prop 表示显示的字段 -->
+      <el-table-column width="600"  label="标题"></el-table-column>
+      <el-table-column label="评论状态"></el-table-column>
+      <el-table-column label="总评论数"></el-table-column>
+      <el-table-column label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
+
+   </el-table>
 </el-card>
 
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,30 +1,30 @@
 <template>
-  <div  class="login">
+  <div class="login">
     <!-- 表单 -->
     <el-card class="login-card">
-     <!-- 头部login部分 -->
-     <div class="title">
-       <img src="../../assets/img/logo_index.png" alt="">
-     </div>
-     <!-- 表单容器  要绑定model属性 rules属性(表单验证规则)-->
-     <!-- 加上:才会把model看成一个变量 -->
-     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:21px;">
-     <!-- 每行都是一个el-form-item -->
-     <el-form-item prop="mobile">
-       <!-- input标签  双向绑定v-model-->
-       <el-input  v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
-     </el-form-item>
-     <el-form-item prop="code">
-       <el-input v-model="loginForm.code" style="width:60%;" placeholder="请输入验证码"></el-input>
-       <el-button style="float:right; width:35%" plain>发送验证码</el-button>
-     </el-form-item>
-     <el-form-item prop="checked">
-       <el-checkbox v-model="loginForm.checked" style="color:#ccc">我已阅读并同意用户协议和隐私条款</el-checkbox>
-     </el-form-item>
-<el-form-item>
-  <el-button @click="login" style="width:100%" type="primary" round>登陆</el-button>
-</el-form-item>
-     </el-form>
+      <!-- 头部login部分 -->
+       <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+       </div>
+      <!-- 表单容器  要绑定model属性 rules属性(表单验证规则)-->
+      <!-- 加上:才会把model看成一个变量 -->
+       <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:21px;">
+        <!-- 每行都是一个el-form-item -->
+          <el-form-item prop="mobile">
+          <!-- input标签  双向绑定v-model-->
+              <el-input v-model="loginForm.mobile" placeholder="请输入手机号" ></el-input>
+          </el-form-item>
+          <el-form-item prop="code">
+              <el-input v-model="loginForm.code" style="width:60%;" placeholder="请输入验证码"></el-input>
+              <el-button style="float:right; width:35%" plain>发送验证码</el-button>
+          </el-form-item>
+          <el-form-item prop="checked">
+              <el-checkbox v-model="loginForm.checked" style="color:#ccc">我已阅读并同意用户协议和隐私条款</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="login" style="width:100%" type="primary" round>登陆</el-button>
+          </el-form-item>
+       </el-form>
     </el-card>
   </div>
 </template>
@@ -37,29 +37,36 @@ export default {
       loginForm: {
         mobile: '', // 手机号
         code: '', // 验证码
-        checked: false// 是否同意用户协议
+        checked: false // 是否同意用户协议
       },
       // 定义表单的验证规则
       loginRules: {
         // 手机号的验证规则
-        mobile: [{ required: true, message: '您的手机号不能为空' },
-        // 正则表达式 以1开头，第二个数字是3-9，后边还有9个数字的格式
-          { pattern: /^1[3-9]\d{9}$/, message: '您的手机号格式不正确' }],
+        mobile: [
+          { required: true, message: '您的手机号不能为空' },
+          // 正则表达式 以1开头，第二个数字是3-9，后边还有9个数字的格式
+          { pattern: /^1[3-9]\d{9}$/, message: '您的手机号格式不正确' }
+        ],
         // 验证码的验证规则
-        code: [{ required: true, message: '您的验证码不能为空' },
-        // 验证码要求六个数字
-          { pattern: /^\d{6}$/, message: '验证码应该是六位数字' }],
+        code: [
+          { required: true, message: '您的验证码不能为空' },
+          // 验证码要求六个数字
+          { pattern: /^\d{6}$/, message: '验证码应该是六位数字' }
+        ],
         // 是否勾选的验证规则
         // required不能验证false和true，为true，是必填项
         // 自定义校验 如果value为true则校验成功
-        checked: [{
-          // rule是当前规则 value是当前要校验的值 callback是一个必须执行的函数，成功的时候执行callback，失败的时候执行callback里面的newError
-          validator: function (rule, value, callback) {
-            // 三元表达式 value为true则成功 如果成功则执行callback（），false则失败，执行callback（new Error（'错误信息'））
-            value ? callback() : callback(new Error('您必须同意我们的霸王条款之后才可以继续'))
+        checked: [
+          {
+            // rule是当前规则 value是当前要校验的值 callback是一个必须执行的函数，成功的时候执行callback，失败的时候执行callback里面的newError
+            validator: function (rule, value, callback) {
+              // 三元表达式 value为true则成功 如果成功则执行callback（），false则失败，执行callback（new Error（'错误信息'））
+              value
+                ? callback()
+                : callback(new Error('您必须同意我们的霸王条款之后才可以继续'))
+            }
           }
-        }]
-
+        ]
       }
     }
   },
@@ -85,20 +92,22 @@ export default {
           data: this.loginForm,
           // 请求类型
           method: 'post'
-        }).then(result => {
-          // 成功之后打印结果
-          // console.log(result.data)
-          // 把token存于本地缓存，用的时候兜里拿 result.data.data.token 解构之前的代码
-          window.localStorage.setItem('user-token', result.data.token)
-          // 跳转到主页
-          // push和router-link类似to属性可以直接是字符串也可以是对象
-          this.$router.push('/home')
-        }).catch(() => {
-          // 第一种用法
-          // this.$message({ message: '用户名或者密码错误', type: 'error' })
-          // 第二种方法
-          this.$message.error('错了哦，这是一条错误消息')
         })
+          .then(result => {
+            // 成功之后打印结果
+            // console.log(result.data)
+            // 把token存于本地缓存，用的时候兜里拿 result.data.data.token 解构之前的代码
+            window.localStorage.setItem('user-token', result.data.token)
+            // 跳转到主页
+            // push和router-link类似to属性可以直接是字符串也可以是对象
+            this.$router.push('/home')
+          })
+          .catch(() => {
+            // 第一种用法
+            // this.$message({ message: '用户名或者密码错误', type: 'error' })
+            // 第二种方法
+            this.$message.error('错了哦，这是一条错误消息')
+          })
       })
     }
   }
@@ -106,9 +115,8 @@ export default {
 </script>
 
 <style lang='less'>
-
 // 如果需要写less 那么就需要在style标签中给lang赋值less
-.login{
+.login {
   background-image: url(../../assets/img/login.jpeg);
   height: 100vh;
   // 完全自适应当前可视区
@@ -117,15 +125,15 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  .login-card{
+  .login-card {
     width: 447px;
     height: 347px;
-    background-color:rgba(0,0,0,0.4);
-    .title{
+    background-color: rgba(0, 0, 0, 0.4);
+    .title {
       // img是行内元素，所以在外边的块级元素给一个text-align：center可居中
       text-align: center;
-      img{
-       height: 56px;
+      img {
+        height: 56px;
       }
     }
   }
