31a447d6d9efb9b2137617d5f6fe8773cfa165e5
xiaojiayao777 <1392684833@qq.com>
2020-03-07 18:35:24 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -8,12 +8,28 @@
           素材管理
         </template>
      </bread-crumb>
+     <!-- 放置标签页 v-model所绑定的值 就是当前所激活的页签-->
+     <el-tabs v-model="activeName">
+        <!--lable表示标签显示的内容  -->
+        <el-tab-pane label="全部" name="all">
+            <!-- 内容 -->
+        </el-tab-pane>
+        <el-tab-pane label="收藏" name="collect">
+            <!-- 内容 -->
+
+        </el-tab-pane>
+     </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 当前激活的页签
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
