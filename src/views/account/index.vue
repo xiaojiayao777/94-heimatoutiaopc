20de941bc7c8107b241d9f861112a18877216493
xiaojiayao777 <1392684833@qq.com>
2020-03-12 22:27:24 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,21 +6,21 @@
      <!-- 放置表单容器 -->
      <el-form label-width="100px">
         <!-- 表单域 -->
-        <el-form-item label="用户名">
-           <el-input style="width:30%">
+        <el-form-item  label="用户名">
+           <el-input  style="width:30%" v-model="formData.name">
            </el-input>
         </el-form-item>
         <el-form-item label="简介">
-           <el-input style="width:30%">
+           <el-input style="width:30%" v-model="formData.intro">
            </el-input>
         </el-form-item>
         <el-form-item label="邮箱">
-           <el-input style="width:30%">
+           <el-input style="width:30%" v-model="formData.email">
            </el-input>
         </el-form-item>
         <el-form-item label="手机号">
            <!-- 可以看到 不能改 是登陆标志 -->
-           <el-input disabled style="width:30%">
+           <el-input disabled style="width:30%" v-model="formData.mobile">
            </el-input>
         </el-form-item>
         <el-form-item label="">
@@ -34,6 +34,31 @@
 
 <script>
 export default {
+  data () {
+    return {
+      formData: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      }
+    }
+  },
+  methods: {
+    // 获取用户个人资料
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        // 将数据赋值给表单数据
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 
 }
 </script>
