02ab28729e29de3693e61f20d2557a35f65662b2
xiaojiayao777 <1392684833@qq.com>
2020-03-12 22:16:48 +0800

    账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,32 @@
      <bread-crumb slot="header">
         <template slot="title"></template>
      </bread-crumb>
+     <!-- 放置表单容器 -->
+     <el-form label-width="100px">
+        <!-- 表单域 -->
+        <el-form-item label="用户名">
+           <el-input style="width:30%">
+           </el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+           <el-input style="width:30%">
+           </el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+           <el-input style="width:30%">
+           </el-input>
+        </el-form-item>
+        <el-form-item label="手机号">
+           <!-- 可以看到 不能改 是登陆标志 -->
+           <el-input disabled style="width:30%">
+           </el-input>
+        </el-form-item>
+        <el-form-item label="">
+            <el-button type="primary">保存</el-button>
+        </el-form-item>
+     </el-form>
+     <!-- 头像 -->
+     <img class="head-upload" src="../../assets/img/13.jpg" alt="">
   </el-card>
 </template>
 
@@ -12,6 +38,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+.head-upload{
+   position: absolute;
+   right: 300px;
+   top: 160px;
+   width: 200px;
+   height: 200px;
+   border-radius: 50%;
+}
 
 </style>
